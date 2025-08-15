6ced6592c37021e5a5c193f67846ca3d1a3a9d3c
Raul Reyes <137012254+RaulReyesB@users.noreply.github.com>
2025-08-14 22:20:38 -0600

    Cambios en la pagina de boletos y mejor estructuracion de la misma
    Se realizaron cambios al diseño adeams de cambiar todo a idioma espanol y mas especificamente espanol latino en los horarios

--- front/src/app/services/tickets.ts
+++ front/src/app/services/tickets.ts
@@ -20,12 +20,17 @@ export class TicketsService {
   private http = inject(HttpClient);
   private base = 'http://localhost:3000/api';
 
-  buscar(origen: string, destino: string, fecha: string): Observable<Viaje[]> {
-    const params = new HttpParams().set('origen', origen).set('destino', destino).set('fecha', fecha);
-    return this.http.get<Viaje[]>(`${this.base}/trips`, { params });
+  buscar(origen: string, destino: string, fecha: string) {
+    const params = new HttpParams()
+      .set('origen', origen)
+      .set('destino', destino)
+      .set('fecha', fecha);
+    return this.http.get<Viaje[]>(`${this.base}/viajes`, { params });
   }
 
-  getCiudades(): Observable<{origins: string[]; destinations: string[]}> {
-    return this.http.get<{origins: string[]; destinations: string[]}>(`${this.base}/rutas/ciudades`);
+  getCiudades(): Observable<{ origins: string[]; destinations: string[] }> {
+    return this.http.get<{ origins: string[]; destinations: string[] }>(
+      `${this.base}/rutas/ciudades`
+    );
   }
 }
